f77003af759179d3cafce3b0167ea4f6965061b4
Andreas Grams <andreas.grams@gmail.com>
2016-03-12 19:46:23 +0100

    add a cellFactory

--- src/main/java/de/dreamnetworx/fxmvp/sample/app/ui/view/PersonSelectorViewImpl.java
+++ src/main/java/de/dreamnetworx/fxmvp/sample/app/ui/view/PersonSelectorViewImpl.java
@@ -1,5 +1,6 @@
 package de.dreamnetworx.fxmvp.sample.app.ui.view;
 
+import de.dreamnetworx.fxmvp.fx.Messages;
 import de.dreamnetworx.fxmvp.sample.app.ui.model.PersonViewModel;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -7,7 +8,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.util.Callback;
 
 import java.net.URL;
 import java.util.List;
@@ -27,6 +30,20 @@ public class PersonSelectorViewImpl implements PersonSelectorView, Initializable
             @Override
             public void changed(final ObservableValue<? extends PersonViewModel> observable, final PersonViewModel oldValue, final PersonViewModel newValue) {
                 viewObserver.onPersonItemSelected(newValue);
+            }
+        });
+        personListView.setCellFactory(new Callback<ListView<PersonViewModel>, ListCell<PersonViewModel>>() {
+            @Override
+            public ListCell<PersonViewModel> call(final ListView<PersonViewModel> param) {
+                return new ListCell<PersonViewModel>(){
+                    @Override
+                    protected void updateItem(final PersonViewModel item, final boolean empty) {
+                        super.updateItem(item, empty);
+                        if(!empty) {
+                            setText(item.getSurname().concat(" ").concat(item.getFirstName()));
+                        }
+                    }
+                };
             }
         });
     }
